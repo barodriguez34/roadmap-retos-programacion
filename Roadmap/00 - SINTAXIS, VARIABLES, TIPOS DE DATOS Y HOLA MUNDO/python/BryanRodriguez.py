7ee02ad772e04a705121f72c24b34332096af76c
barodriguez34 <63655041+barodriguez34@users.noreply.github.com>
2024-08-28 20:49:02 -0500

    Create BryanRodriguez.py

---
+++ Roadmap/00 - SINTAXIS, VARIABLES, TIPOS DE DATOS Y HOLA MUNDO/python/BryanRodriguez.py
@@ -0,0 +1,23 @@
+#Enlace oficial de Python https://www.python.org/
+
+#Los comentarios se pueden realizar con comodines o comillas (#, """...""" o '''...''')
+
+#Crear variables
+a = 'X'
+
+#Tipos de variables
+#Cadena
+s = "Hola"
+
+#Numero
+n = 1
+
+#Flotante
+f = 1.5
+
+#Booleano
+b = true
+
+#Impresión de la palabra Hola
+l = "Python"
+print ("!Hola, " + l)
